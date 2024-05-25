e8ebc9602dabcdc371cd009ec69e9fdd1ec75299
peedekza40 <peedekza40@hotmail.com>
2024-05-26 02:07:49 +0700

    Setting debug launch for angulare and edit nav menu data.

--- practiceangular.client/src/app/layouts/full/sidebar/sidebar-data.ts
+++ practiceangular.client/src/app/layouts/full/sidebar/sidebar-data.ts
@@ -5,37 +5,9 @@ export const navItems: NavItem[] = [
     navCap: 'Home',
   },
   {
-    displayName: 'Dashboard',
+    displayName: 'Home',
     iconName: 'layout-dashboard',
-    route: '/dashboard',
-  },
-  {
-    navCap: 'Ui Components',
-  },
-  {
-    displayName: 'Badge',
-    iconName: 'rosette',
-    route: '/ui-components/badge',
-  },
-  {
-    displayName: 'Chips',
-    iconName: 'poker-chip',
-    route: '/ui-components/chips',
-  },
-  {
-    displayName: 'Lists',
-    iconName: 'list',
-    route: '/ui-components/lists',
-  },
-  {
-    displayName: 'Menu',
-    iconName: 'layout-navbar-expand',
-    route: '/ui-components/menu',
-  },
-  {
-    displayName: 'Tooltips',
-    iconName: 'tooltip',
-    route: '/ui-components/tooltips',
+    route: '/home',
   },
   {
     navCap: 'Auth',
@@ -49,18 +21,5 @@ export const navItems: NavItem[] = [
     displayName: 'Register',
     iconName: 'user-plus',
     route: '/authentication/register',
-  },
-  {
-    navCap: 'Extra',
-  },
-  {
-    displayName: 'Icons',
-    iconName: 'mood-smile',
-    route: '/extra/icons',
-  },
-  {
-    displayName: 'Sample Page',
-    iconName: 'aperture',
-    route: '/extra/sample-page',
   },
 ];
--- practiceangular.client/src/proxy.conf.js
+++ practiceangular.client/src/proxy.conf.js
@@ -1,12 +1,12 @@
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:5001';
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7146';
 
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
+      "/api",
     ],
     target,
     secure: false
